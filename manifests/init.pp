61e25e3eb5d07f6ccdba5983de3542b5195740cc
Rehan Mahmood <rehanone@gmail.com>
2021-10-26 16:32:48 -0400

    Added `repo_ensure` parameter to control the vcsrepo status.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,3 @@
-
 # Setup dockctl scripts
 class dockctl (
   String                $git_package,
@@ -7,13 +6,15 @@ class dockctl (
   Enum[present, absent] $ensure,
   Stdlib::Httpsurl      $repo_source,
   String                $repo_revision,
+  Enum[present, bare, mirror, absent, latest]
+  $repo_ensure,
   Stdlib::Absolutepath  $install_directory,
   Stdlib::Absolutepath  $home_directory,
 ) {
 
   anchor { "${module_name}::begin": }
-  -> class {"${module_name}::dependencies":}
-  -> class {"${module_name}::config":}
-  -> class {"${module_name}::install":}
+  -> class { "${module_name}::dependencies": }
+  -> class { "${module_name}::config": }
+  -> class { "${module_name}::install": }
   -> anchor { "${module_name}::end": }
 }
